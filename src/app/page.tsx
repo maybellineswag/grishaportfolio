2628005b165eff01822d208c503b40203777381b
maybellineswag <advantagesofkindness@gmail.com>
2026-04-10 04:42:07 +0200

    Quick update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -119,13 +119,13 @@ export default function Portfolio() {
             
             <Reveal className="block md:hidden mt-[28px] text-[#111111] font-normal space-y-2" stagger={0.1} threshold={0}>
                <a href="https://t.me/maybellineswag" target="_blank" rel="noopener noreferrer" className="flex items-center gap-3 group -m-2 p-2 w-fit">
-                 <div className="w-[18px] flex justify-center">
+                 <div className="w-[18px] flex justify-center translate-y-[1px]">
                    <TelegramIcon className="text-amber" />
                  </div>
                  <span className="group-hover:text-amber transition-colors">maybellineswag</span>
                </a>
-               <a href="mailto:advantagesofkindness@gmail.com" className="flex items-center gap-3 group -m-2 p-2 w-fit">
-                 <div className="w-[18px] flex justify-center">
+               <a href="mailto:advantagesofkindness@gmail.com?subject=We%20searched%20the%20whole%20internet%20and%20found%20you&body=Hello%20Grisha%2C%0A%0AWe%20came%20across%20your%20work%20and%20honestly%20we%20weren't%20expecting%20to%20be%20this%20impressed.%20We%20have%20an%20open%20position%20%2F%20project%20that%20feels%20like%20it%20was%20made%20for%20you%20%E2%80%94%20good%20pay%2C%20interesting%20work%2C%20and%20people%20who%20actually%20have%20taste.%0AWould%20love%20to%20get%20on%20call%20and%20tell%20you%20more.%0A%0ALooking%20forward%20to%20hearing%20from%20you%20Grisha!" className="flex items-center gap-3 group -m-2 p-2 w-fit">
+                 <div className="w-[18px] flex justify-center translate-y-[1px]">
                    <MailIcon className="text-amber" />
                  </div>
                  <span className="group-hover:text-amber transition-colors">advantagesofkindness@gmail.com</span>
@@ -183,8 +183,8 @@ export default function Portfolio() {
                        advantagesofkindness@gmail.com
                      </span>
                      
-                     {/* ABSOLUTE FEEDBACK */}
-                     <div className="absolute left-full ml-[12px] flex items-center whitespace-nowrap pointer-events-none group-hover:pointer-events-auto">
+                     {/* ABSOLUTE FEEDBACK - Using pl instead of ml to create a hover bridge */}
+                     <div className="absolute left-full pl-[12px] flex items-center h-full whitespace-nowrap pointer-events-none group-hover:pointer-events-auto">
                         <div className="overflow-hidden transition-all duration-300 max-w-0 opacity-0 group-hover:max-w-[120px] group-hover:opacity-100 flex items-center">
                           <a 
                             href="https://mail.google.com/mail/?view=cm&fs=1&to=advantagesofkindness@gmail.com&su=We+searched+the+whole+internet+and+found+you&body=Hello+Grisha%2C%0A%0AWe+came+across+your+work+and+honestly+we+weren%27t+expecting+to+be+this+impressed.+We+have+an+open+position+/+project+that+feels+like+it+was+made+for+you+%E2%80%94+good+pay%2C+interesting+work%2C+and+people+who+actually+have+taste.%0AWould+love+to+get+on+a+call+and+tell+you+more.%0A%0ALooking+forward+to+hearing+from+you+Grisha%21" 
